447eb78a8dbfa01240d0939c5ba7582a9d40e498
Mahim <mahimmasrafi04@gmail.com>
2024-07-29 21:44:31 +0600

    updated aesthetics

--- public/script.js
+++ public/script.js
@@ -1,83 +1,89 @@
 // io() connects to the socket.io server at the url specified
-const socket = io('http://localhost:4000',{
-    reconnectionAttempts: Infinity, // we want to keep trying to reconnect indefinitely
-    reconnectionDelay: 1000 ,// we want to wait 1 second before trying to reconnect
+const socket = io("http://localhost:4000", {
+  reconnectionAttempts: Infinity, // we want to keep trying to reconnect indefinitely
+  reconnectionDelay: 1000, // we want to wait 1 second before trying to reconnect
 });
 // ----------------------------------------------------------------
 // username settings
 // let username = '';
 // if already exists a user:
-let username = localStorage.getItem('username') || '';
+let username = localStorage.getItem("username") || "";
 
-document.addEventListener('DOMContentLoaded', () => {
-    if (username) {
-        document.getElementById('usernameForm').style.display = 'none';
-        document.getElementById('chatForm').style.display = 'flex';
-        socket.emit('newUser', username);
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  if (username) {
+    document.getElementById("usernameForm").style.display = "none";
+    document.getElementById("chatForm").style.display = "flex";
+    socket.emit("newUser", username);
+  }
 });
 // chat history check
 
-socket.on('chatHistory', history => {
-    const messageList = document.getElementById('messages');
-    history.forEach(({ user, msg }) => {
-        const newMessage = document.createElement('li');
-        newMessage.innerHTML = `<span class="font-bold text-red-500">${user}</span>: ${msg}`;
-        messageList.appendChild(newMessage);
-    });
-});
-
-socket.on('userJoined', msg => {
-    const messageList = document.getElementById('messages');
-    const newMessage = document.createElement('li');
-    newMessage.innerHTML = `<span class="font-bold text-green-500 ">${msg}</span>`;
+socket.on("chatHistory", (history) => {
+  const messageList = document.getElementById("messages");
+  history.forEach(({ user, msg }) => {
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = `<span class="font-bold text-red-500">${user}</span>: ${msg}`;
     messageList.appendChild(newMessage);
+  });
 });
 
-socket.on('userLeft', msg => {
-    const messageList = document.getElementById('messages');
-    const newMessage = document.createElement('li');
-    newMessage.innerHTML = `<span class="font-bold text-gray-500 ">${msg}</span>`;
-    
-    messageList.appendChild(newMessage);
+socket.on("userJoined", (msg) => {
+  const messageList = document.getElementById("messages");
+  const newMessage = document.createElement("li");
+  newMessage.classList.add("text-center");
+  newMessage.innerHTML = `<span class="font-bold text-green-500 ">${msg}</span>`;
+  messageList.appendChild(newMessage);
 });
 
-document.getElementById('setUsernameBtn').addEventListener('click', e => {
-    e.preventDefault();
-    username = document.getElementById('usernameInput').value.trim();
-    if (username) {
-        document.getElementById('usernameForm').style.display = 'none';
-        document.getElementById('chatForm').style.display = 'flex';
-        socket.emit('newUser', username);
-    } else {
-        alert('Please enter a username.');
-    }
+socket.on("userLeft", (msg) => {
+  const messageList = document.getElementById("messages");
+  const newMessage = document.createElement("li");
+  newMessage.classList.add("text-center");
+  newMessage.innerHTML = `<span class="font-bold text-gray-500 ">${msg}</span>`;
+
+  messageList.appendChild(newMessage);
+});
+
+document.getElementById("setUsernameBtn").addEventListener("click", (e) => {
+  e.preventDefault();
+  username = document.getElementById("usernameInput").value.trim();
+  if (username) {
+    document.getElementById("usernameForm").style.display = "none";
+    document.getElementById("chatForm").style.display = "flex";
+    socket.emit("newUser", username);
+  } else {
+    alert("Please enter a username.");
+  }
 });
 
 // just like the server, our socket has an on method and an emit method
-socket.on('welcome', data=>{
-    console.log(data)
-    // emit a message to the server with the client's username
-    socket.on('newUser', data=>{console.log('A new client has joined:',data)})
-})
-socket.on('messageFromServerToAll', ({ user, msg })=>{
-    document.getElementById('messages').innerHTML += `<li class="p-1 border-[1px] rounded-md w-max border-gray-300 mb-1"> <span class="font-bold text-red-500">${user}</span>: ${msg}</li>`
-})
-
-// Message sent to the server
-const sendBtn= document.getElementById('sendMessageBtn');
-sendBtn.addEventListener('click',e=>{
-    e.preventDefault();
-    const message=document.getElementById('messageInput').value;
-    document.getElementById('messageInput').value='';
-    socket.emit('sendMessageToServer', { user: username, msg: message })
-})
-
-// Handle connection errors
-socket.on('connect_error', () => {
-    alert('Connection failed. Please try again later.');
+socket.on("welcome", (data) => {
+  console.log(data);
+  // emit a message to the server with the client's username
+  socket.on("newUser", (data) => {
+    console.log("A new client has joined:", data);
+  });
+});
+socket.on("messageFromServerToAll", ({ user, msg }) => {
+  document.getElementById(
+    "messages"
+  ).innerHTML += `<li class="p-1 border-[1px] rounded-md w-max border-gray-300 mb-1 text-right"> <span class="font-bold text-red-500">${user}</span>: ${msg}</li>`;
 });
 
-socket.on('disconnect', (reason) => {
-    console.log(reason); // "ping timeout"
+// Message sent to the server
+const sendBtn = document.getElementById("sendMessageBtn");
+sendBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const message = document.getElementById("messageInput").value;
+  document.getElementById("messageInput").value = "";
+  socket.emit("sendMessageToServer", { user: username, msg: message });
+});
+
+// Handle connection errors
+socket.on("connect_error", () => {
+  alert("Connection failed. Please try again later.");
+});
+
+socket.on("disconnect", (reason) => {
+  console.log(reason); // "ping timeout"
 });
